0491a9c10cc7152186c25e6ae64e73ac272b3b94
Pawan <pbilkhu15@gmail.com>
2021-03-11 08:51:53 -0800

    Fixed computation in Q3.py from multiplication to power

--- Assignments/A3/Q3.py
+++ Assignments/A3/Q3.py
@@ -4,15 +4,14 @@ def main():
         generatorClass = []
         print("----", residue, "----")
         for i in range(0, 25, 1):
-            (q, r) = divmod(residue * i, 18)
+            (q, r) = divmod(residue ** i, 18)
 
             if r not in generatorClass and r in residueClass:
-                print(residue, "x", i, "is congruent to: ", r)
+                print(residue, "^", i, "is congruent to: ", r)
                 generatorClass.append(r)
         generatorClass.sort()
         print(generatorClass)
 
 
 if __name__ == "__main__":
-    # execute only if run as a script
     main()
